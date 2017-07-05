a2930d8bea7557bd9605de601cec05c02b1dc29a
yangtong123 <542919899@qq.com>
2017-07-05 21:13:48 +0800

    二叉树

--- src/yt/codingoffer/utils/BinaryTree.java
+++ src/yt/codingoffer/utils/BinaryTree.java
@@ -1,7 +1,7 @@
 package yt.codingoffer.utils;
 
 public class BinaryTree {
-	public static BinaryTreeNode createBinaryTreeNode(int value) {
+	public static BinaryTreeNode CreateBinaryTreeNode(int value) {
 		BinaryTreeNode pNode = new BinaryTreeNode();
 		pNode.value = value;
 		pNode.left = null;
@@ -10,43 +10,44 @@ public class BinaryTree {
 		return pNode;
 	}
 
-	public static void connectTreeNodes(BinaryTreeNode pParent, BinaryTreeNode pLeft, BinaryTreeNode pRight) {
+	public static void ConnectTreeNodes(BinaryTreeNode pParent, BinaryTreeNode pLeft, BinaryTreeNode pRight) {
 		if (pParent != null) {
 			pParent.left = pLeft;
 			pParent.right = pRight;
 		}
 	}
 
-	public static void printTreeNode(BinaryTreeNode pNode) {
+	public static void PrintTreeNode(BinaryTreeNode pNode) {
 		if (pNode != null) {
-			System.out.printf("value of this node is: %d\n", pNode.value);
+//			System.out.printf("value of this node is: lf\n", pNode.value);
+			System.out.println("value of this node is: " + pNode.value);
 
 			if (pNode.left != null)
-				System.out.printf("value of its left child is: %d.\n", pNode.left.value);
+				System.out.println("value of its left child is: " + pNode.left.value);
 			else
-				System.out.printf("left child is nullptr.\n");
+				System.out.println("left child is null");
 
 			if (pNode.right != null)
-				System.out.printf("value of its right child is: %d.\n", pNode.right.value);
+				System.out.println("value of its right child is: " + pNode.right.value);
 			else
-				System.out.printf("right child is nullptr.\n");
+				System.out.println("right child is null");
 		} else {
-			System.out.printf("this node is nullptr.\n");
+			System.out.println("this node is null");
 		}
 
 		// printf("\n");
 		System.out.println();
 	}
 
-	public static void printTree(BinaryTreeNode pRoot) {
-		printTreeNode(pRoot);
+	public static void PrintTree(BinaryTreeNode pRoot) {
+		PrintTreeNode(pRoot);
 
 		if (pRoot != null) {
 			if (pRoot.left != null)
-				printTree(pRoot.left);
+				PrintTree(pRoot.left);
 
 			if (pRoot.right != null)
-				printTree(pRoot.right);
+				PrintTree(pRoot.right);
 		}
 	}
 }
