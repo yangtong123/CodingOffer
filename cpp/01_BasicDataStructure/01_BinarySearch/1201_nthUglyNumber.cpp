1487ba03cfe01e810382560690d762c62b486bbb
Yang Tong <yangtong.hust@gmail.com>
2022-09-03 22:59:10 +0800

    1201. 丑数 III

---
+++ cpp/01_BasicDataStructure/01_BinarySearch/1201_nthUglyNumber.cpp
@@ -0,0 +1,62 @@
+//
+// Created by yangtong on 2022/9/3.
+//
+
+class Solution {
+public:
+    int nthUglyNumber(int n, int a, int b, int c) {
+        int left = 0, right = (int) 2e9;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (f(mid, a, b, c) < n) {
+                left = mid + 1;
+            } else{
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+
+    long f(int num, int a, int b, int c) {
+        long setA = num / a;
+        long setB = num / b;
+        long setC = num / c;
+
+        long setAB = num / lcm(a, b);
+        long setAC = num / lcm(a, c);
+        long setBC = num / lcm(b, c);
+        long setABC = num / lcm(lcm(a, b), c);
+
+        return setA + setB + setC - setAB - setAC - setBC + setABC;
+    }
+
+    long lcm(long a, long b) {
+        return a * b / gcd(a, b);
+    }
+
+    long gcd(long a, long b) {
+        if (a < b) {
+            gcd(b, a);
+        }
+
+        if (b == 0) {
+            return a;
+        }
+
+        return gcd(b, a % b);
+    }
+};
+
+int main() {
+    int n = 4;
+    int a = 2;
+    int b = 3;
+    int c = 4;
+
+    Solution solution;
+
+    solution.nthUglyNumber(n, a, b, c);
+}
+
---
+++ cpp/02_DynamicProgramming/494_FindTargetSumWays.cpp
@@ -0,0 +1,10 @@
+//
+// Created by yangtong on 2022/9/3.
+//
+#include <vector>
+
+using namespace std;
+
+int findTargetSumWays(vector<int>& nums, int target) {
+
+}
