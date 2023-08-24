1faa6b66f916f9f25aa43d9b8c8dba3afd79d987
yangtong <542919899@qq.com>
2023-08-24 22:52:16 +0800

    init java

---
+++ java/leetcode/src/main/java/com/yln/yt/N141_LinkedListCycle.java
@@ -0,0 +1,7 @@
+package com.yln.yt;
+
+public class N141_LinkedListCycle {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ java/leetcode/src/main/java/common/ListNode.java
@@ -0,0 +1,10 @@
+package common;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
