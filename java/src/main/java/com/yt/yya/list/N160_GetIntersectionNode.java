b63f216923f8f9e03d74b998470afcedfcd26719
Yang Tong <yangtong.hust@gmail.com>
2023-08-28 23:59:16 +0800

    相交链表

---
+++ java/src/main/java/com/yt/yya/list/N160_GetIntersectionNode.java
@@ -0,0 +1,19 @@
+package com.yt.yya.list;
+
+import com.yt.yya.utils.ListNode;
+
+public class N160_GetIntersectionNode {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+
+        ListNode pa = headA, pb = headB;
+        while (pa != pb) {
+            pa = pa == null ? headB : pa.next;
+            pb = pb == null ? headA : pb.next;
+        }
+
+        return pa;
+    }
+}
